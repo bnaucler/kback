1c4ae0fcd646920586126559b2ef074f89f3288e
bnaucler <mail@bnaucler.se>
2017-07-25 21:36:50 +0200

    Increasing type safety

--- kback.c
+++ kback.c
@@ -7,7 +7,7 @@
 #define MAXF "/sys/class/backlight/intel_backlight/max_brightness"
 #define BRIF "/sys/class/backlight/intel_backlight/brightness"
 
-int usage(const char *cn, const int max, FILE *f) {
+static int usage(const char *cn, const int max, FILE *f) {
 
 	if(errno) fprintf(stderr, "Error: %s\n", strerror(errno));
 
@@ -17,73 +17,69 @@ int usage(const char *cn, const int max, FILE *f) {
 	return errno;
 }
 
-int mkint(const char *str) {
+static int matoi(const char *str) {
 
 	char *ptr;
 
 	long ret = strtol(str, &ptr, 10);
-	if (str == ptr) errno = EINVAL;
+	if(str == ptr) errno = EINVAL;
 
-	return (int) ret;
+	return (int)ret;
 }
 
-int getval(FILE *f) {
+static int getval(FILE *f) {
 
-	char fbuf[MBCH] = "";
+	char fbuf[MBCH];
+	fgets(fbuf, MBCH, f);
 
-	if (!f) errno = ENOENT;
-	else fgets(fbuf, MBCH, f);
-
-	return mkint(fbuf);
+	return matoi(fbuf);
 }
 
-int sfix(char *str, int cur, int max) {
+static int sfix(const char *str, const int cur, const int max) {
 
-	if (!strncmp(str, "max", MBCH)) return max;
-	else if (str[0] == '+' || str[0] == '-') return (cur + mkint(str));
-	else return mkint(str);
+	if(!strncmp(str, "max", MBCH)) return max;
+	else if(str[0] == '+' || str[0] == '-') return (cur + matoi(str));
+	else return matoi(str);
 }
 
-int sper(char *str, int cur, int max) {
+static int sper(const char *str, const int cur, const int max) {
 
-	if (str[0] == '+' || str[0] == '-')
-		 return(int) ((float)mkint(str)/100 * (float)max) + (float)cur;
-	else return(int) ((float)mkint(str)/100 * (float)max);
+	if(str[0] == '+' || str[0] == '-')
+		return(int) ((float)matoi(str)/100 * (float)max) + (float)cur;
+	else return(int) ((float)matoi(str)/100 * (float)max);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
 
-	int max = 0, cur = 0;
+	int max = 0, cur = 0, nval = 0;
+	FILE *mf, *bf;
 
-	FILE *mf = fopen(MAXF, "r");
-	if(!errno) max = getval(mf);
+	if((mf = fopen(MAXF, "r"))) max = getval(mf);
 	fclose(mf);
 
-	FILE *bf = fopen(BRIF, "w+");
-	if(!errno) cur = getval(bf);
+	if((bf = fopen(BRIF, "w+"))) cur = getval(bf);
 
-	if (errno) return usage(argv[0], max, bf);
+	if(errno) return usage(argv[0], max, bf);
 
-	if (argc > 2) {
+	if(argc > 2) {
 		return usage(argv[0], max, bf);
 
-	} else if (argc == 1 && !errno) {
+	} else if(argc == 1) {
 		printf("cur: %d (%d%%), max: %d\n",
 			cur, (int)(((float)cur / (float)max) * 100), max);
 		return 0;
 	}
 
-	int setnew = 0;
+	if(argv[1][strlen(argv[1])-1] == '%') nval = sper(argv[1], cur, max);
+	else nval = sfix(argv[1], cur, max);
 
-	if (argv[1][strlen(argv[1])-1] == '%') setnew = sper(argv[1], cur, max);
-	else setnew = sfix(argv[1], cur, max);
-	if (setnew > max) setnew = max;
-	else if (setnew < 0) setnew = 0;
+	if(nval > max) nval = max;
+	else if(nval < 0) nval = 0;
 
-	if (errno) return usage(argv[0], max, bf);
+	if(errno) return usage(argv[0], max, bf);
 
-	fprintf(bf, "%d", setnew);
-
+	fprintf(bf, "%d", nval);
 	fclose(bf);
+
 	return 0;
 }
