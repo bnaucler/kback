9b16500251206057c1ad9378b2e306361d51f907
bnaucler <mail@bnaucler.se>
2017-07-27 18:05:53 +0200

    Avoid floating point calculations

--- kback.c
+++ kback.c
@@ -3,31 +3,31 @@
 #include <string.h>
 #include <errno.h>
 
-#define MBCH 16
+#define MBCH 8
 #define MAXF "/sys/class/backlight/intel_backlight/max_brightness"
 #define BRIF "/sys/class/backlight/intel_backlight/brightness"
 
-static int usage(const char *cn, const int max, FILE *f) {
+static long usage(const char *cn, const long max, FILE *f) {
 
 	if(errno) fprintf(stderr, "Error: %s\n", strerror(errno));
 
-	printf("Usage: %s [[0-%d]|[[+|-]0-100%%]|max]\n", cn, max);
+	printf("Usage: %s [[0-%ld]|[[+|-]0-100%%]|max]\n", cn, max);
 	fclose(f);
 
 	return errno;
 }
 
-static int matoi(const char *str) {
+static long matoi(const char *str) {
 
 	char *ptr;
 
 	long ret = strtol(str, &ptr, 10);
 	if(str == ptr) errno = EINVAL;
 
-	return (int)ret;
+	return ret;
 }
 
-static int getval(FILE *f) {
+static long getval(FILE *f) {
 
 	char fbuf[MBCH];
 	fgets(fbuf, MBCH, f);
@@ -35,48 +35,47 @@ static int getval(FILE *f) {
 	return matoi(fbuf);
 }
 
-static int sfix(const char *str, const int cur, const int max) {
+static long sfix(const char *str, const long cur, const long max) {
 
 	if(!strncmp(str, "max", MBCH)) return max;
-	else if(str[0] == '+' || str[0] == '-') return (cur + matoi(str));
+	else if(str[0] == '+' || str[0] == '-') return cur + matoi(str);
 	else return matoi(str);
 }
 
-static int sper(const char *str, const int cur, const int max) {
+static long sper(const char *str, const long cur, const long max) {
 
-	if(str[0] == '+' || str[0] == '-')
-		return(int) ((float)matoi(str)/100 * (float)max) + (float)cur;
-	else return(int) ((float)matoi(str)/100 * (float)max);
+	return str[0] == '+' || str[0] == '-' ?
+		(matoi(str) * max) / 100 + cur:
+		(matoi(str) * max) / 100;
 }
 
 int main(int argc, char **argv) {
 
-	int max = 0, cur = 0, nval = 0;
+	long max = 0, cur = 0, nval = 0;
 	FILE *mf, *bf;
 
 	if((mf = fopen(MAXF, "r"))) max = getval(mf);
-	fclose(mf);
-
 	if((bf = fopen(BRIF, "w+"))) cur = getval(bf);
+	fclose(mf);
 
 	if(errno || argc > 2) {
 		return usage(argv[0], max, bf);
 
 	} else if(argc == 1) {
-		printf("cur: %d (%d%%), max: %d\n",
-			cur, (int)(((float)cur / (float)max) * 100), max);
+		printf("cur: %ld (%ld%%), max: %ld\n", cur, (cur * 100) / max, max);
 		return 0;
 	}
 
-	if(argv[1][strlen(argv[1])-1] == '%') nval = sper(argv[1], cur, max);
-	else nval = sfix(argv[1], cur, max);
+	nval = (argv[1][strlen(argv[1])-1] == '%') ?
+		sper(argv[1], cur, max):
+		sfix(argv[1], cur, max);
 
 	if(errno) return usage(argv[0], max, bf);
 
 	if(nval > max) nval = max;
 	else if(nval < 0) nval = 0;
 
-	fprintf(bf, "%d", nval);
+	fprintf(bf, "%ld", nval);
 	fclose(bf);
 
 	return 0;
